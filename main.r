e718eb17c293aafa2ef5c39090b6669a6ce09c53
Janez Sedeljsak <janez.sedeljsak@gmail.com>
2021-11-26 15:45:12 +0100

    basic model for region

--- main.r
+++ main.r
@@ -1,4 +1,25 @@
-setdw("D:\\ai-heating-model")
+setwd("D:\\ai-heating-consumption")
 
-training_data = read.table("ucnaSem1.txt", header=T, sep=",", stringsAsFactors=T)
-summary(training_data)
+train <- read.table("ucnaSem1.txt", header = T, sep = ",", stringsAsFactors = T)
+test <- read.table("testnaSem1.txt", header = T, sep = ",", stringsAsFactors = T)
+summary(train)
+
+set.seed(0)
+
+library(rpart)
+dt <- rpart(regija ~ ., data = train)
+
+library(rpart.plot)
+rpart.plot(dt)
+
+observed <- test$regija
+predicted <- predict(dt, test, type = "class")
+
+tab <- table(observed, predicted)
+
+CA <- function(obs, pred) {
+	tab <- table(obs, pred)
+	sum(diag(tab)) / sum(tab)
+}
+
+CA(observed, predicted)
