9b18dca8781b1ba68c3aa6af6106668f64b57e96
Janez Sedeljsak <janez.sedeljsak@gmail.com>
2021-11-26 17:13:36 +0100

    added is_weekend & season to data

--- main.r
+++ main.r
@@ -1,11 +1,44 @@
 setwd("D:\\ai-heating-consumption")
 
-train <- read.table("ucnaSem1.txt", header = T, sep = ",", stringsAsFactors = T)
-test <- read.table("testnaSem1.txt", header = T, sep = ",", stringsAsFactors = T)
-summary(train)
+train <- read.csv("ucnaSem1.txt", stringsAsFactors = T)
+test <- read.csv("testnaSem1.txt", stringsAsFactors = T)
+
+# install.packages("lubridate")
+library(lubridate)
+
+IS_WEEKEND <- function(d) {
+    week_day <- wday(d)
+    ifelse(week_day == 1 | week_day == 7, 1, 0)
+}
+
+GET_SEASON <- function(d) {
+    numeric_date <- 100 * month(d) + day(d)
+    cuts <- base::cut(numeric_date, breaks = c(0,319,0620,0921,1220,1231))
+    levels(cuts) <- c("Winter","Spring","Summer","Fall","Winter")
+    cuts
+}
+
+ADD_COLUMNS <- function(t) {
+    dates <- t$datum
+    weeked_rows <- c()
+    season_rows <- c()
+    i <- 1
+    for (d in dates) {
+        weeked_rows[i] <- IS_WEEKEND(d)
+        season_rows[i] <- GET_SEASON(d)
+        i <- i + 1
+    }
+
+    t$is_weekend = weeked_rows
+    t$season = season_rows
+    t
+}
+
+train <- ADD_COLUMNS(train)
+test <- ADD_COLUMNS(test)
+#summary(train)
 
 set.seed(0)
-
 library(rpart)
 dt <- rpart(regija ~ ., data = train)
 
